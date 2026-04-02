450aa9e93a74cde7cf3f394b1eb5cc678c170a93
mskim9097 <mskim9097@gmail.com>
2026-04-02 13:44:53 -0700

    added swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use OpenApi\Attributes as OA;
 
-#[OA\Info(
-    title: "Laravel Students API",
-    version: "1.0.0",
-    description: "API for managing students",
-    contact: new OA\Contact(email: "admin@example.com")
-)]
+#[
+    OA\Info(version: "1.0.0", description: "School API Documentation", title: "School API Documentation"),
+    OA\Server(url: 'http://127.0.0.1:8000/api', description: "local server"), 
+    OA\Server(url: 'https://laravel-sqlite-api-swagger-mze.azurewebsites.net/api', description: "production server"),
+    OA\Server(url: 'https://laravel-sqlite-api-swagger-mze.azurewebsites.net/api', description: "staging server"),
+    OA\SecurityScheme(securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header"),
+]
 abstract class Controller
 {
     //
--- startup.sh
+++ startup.sh
@@ -32,6 +32,8 @@ php /home/site/wwwroot/artisan config:cache
 php /home/site/wwwroot/artisan route:clear
 php /home/site/wwwroot/artisan view:clear
 php /home/site/wwwroot/artisan view:cache
+php /home/site/wwwroot/artisan l5-swagger:publish
+php /home/site/wwwroot/artisan l5-swagger:generate
 
 # Copy default Nginx configuration
 cp /home/site/wwwroot/default /etc/nginx/sites-available/default
